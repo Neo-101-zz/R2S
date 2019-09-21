5ef1608aec6120f83489787e168cf69936d38e83
Ubuntu <ubuntu@ip-172-31-37-204.us-east-2.compute.internal>
2019-09-21 11:23:20 +0000

    Try to read ERA5 reanalysis.

--- swfusion/era5.py
+++ swfusion/era5.py
@@ -3,6 +3,7 @@ import logging
 import os
 
 import cdsapi
+import pygrib
 from sqlalchemy.ext.declarative import declarative_base
 
 import ibtracs
@@ -24,6 +25,7 @@ class ERA5Manager(object):
         self.years = [x for x in range(self.period[0].year,
                                        self.period[1].year+1)]
         self.main_hours = [0, 6, 12, 18]
+        self.cdsapi_client = cdsapi.Client()
         self.vars = [
             'geopotential','relative_humidity','temperature',
             'u_component_of_wind','v_component_of_wind'
@@ -35,33 +37,51 @@ class ERA5Manager(object):
             '925','975','1000'
         ]
         utils.setup_database(self, Base)
-        self.download()
+        self.download_and_read()
 
-    def download(self):
+    def download_majority(self, file_name, year, month):
+        self.cdsapi_client.retrieve(
+            'reanalysis-era5-pressure-levels',
+            {
+                'product_type':'reanalysis',
+                'format':'grib',
+                'variable':self.vars,
+                'pressure_level':self.pres_lvl,
+                'year':f'{year}',
+                'month':str(month).zfill(2),
+                'day':list(self.dt_majority[year][month]['day']),
+                'time':list(self.dt_majority[year][month]['time'])
+            },
+            file_name)
+
+    def download_minority(self, file_name, year, month, day_str):
+        self.cdsapi_client.retrieve(
+            'reanalysis-era5-pressure-levels',
+            {
+                'product_type':'reanalysis',
+                'format':'grib',
+                'variable':self.vars,
+                'pressure_level':self.pres_lvl,
+                'year':f'{year}',
+                'month':str(month).zfill(2),
+                'day':day_str,
+                'time':list(self.dt_minority[year][month][day_str])
+            },
+            file_name)
+
+    def download_and_read(self):
         self._get_target_datetime()
         majority_dir = self.CONFIG['era5']['dirs']['rea_pres_lvl_maj']
         minority_dir = self.CONFIG['era5']['dirs']['rea_pres_lvl_min']
         os.makedirs(majority_dir, exist_ok=True)
         os.makedirs(minority_dir, exist_ok=True)
 
-        c = cdsapi.Client()
-
         for year in self.dt_majority.keys():
             for month in self.dt_majority[year].keys():
                 file_name = f'{majority_dir}{year}{str(month).zfill(2)}.grib'
-                c.retrieve(
-                    'reanalysis-era5-pressure-levels',
-                    {
-                        'product_type':'reanalysis',
-                        'format':'grib',
-                        'variable':self.vars,
-                        'pressure_level':self.pres_lvl,
-                        'year':f'{year}',
-                        'month':str(month).zfill(2),
-                        'day':list(self.dt_majority[year][month]['day']),
-                        'time':list(self.dt_majority[year][month]['time'])
-                    },
-                    file_name)
+                self.download_majority(file_name, year, month)
+                self.read(file_name)
+                os.remove(file_name)
 
         for year in self.dt_minority.keys():
             for month in self.dt_minority[year].keys():
@@ -69,20 +89,26 @@ class ERA5Manager(object):
                     file_name =\
                             (f'{minority_dir}{year}{str(month).zfill(2)}'
                              + f'{day_str}.grib')
-                    c.retrieve(
-                        'reanalysis-era5-pressure-levels',
-                        {
-                            'product_type':'reanalysis',
-                            'format':'grib',
-                            'variable':self.vars,
-                            'pressure_level':self.pres_lvl,
-                            'year':f'{year}',
-                            'month':str(month).zfill(2),
-                            'day':day_str,
-                            'time':list(self.dt_minority[year][month][day_str])
-                        },
-                        file_name)
+                    self.download_minority(file_name, year, month, day_str)
+                    self.read(file_name)
+                    os.remove(file_name)
 
+    def read(self, file_name):
+        # load grib file
+        grbs = pygrib.open(file_name)
+        # loop TC table
+        tc_query = self.session.query(ibtracs.WMOWPTC)
+        total = tc_query.count()
+        count = 0
+        # get lat and lon of row
+        for row in tc_query:
+
+        # search corresponding cell in ERA5 reanalysis file
+        # read out variables
+        # write into DB
+
+    def _get_subset_range_of_grib(self, lat, lon):
+        lat_ae = [abs(lat-x) for x in self.lats_grid_p]
     def _update_majority_datetime_dict(self, dt_dict, year, month, day, hour):
         day_str = str(day).zfill(2)
         time_str = f'{str(hour).zfill(2)}:00'
--- swfusion/manager.py
+++ swfusion/manager.py
@@ -42,7 +42,7 @@ def work_flow():
     passwd = '39cnj971hw-'
     # Download and read
     try:
-        ibtracs_ = ibtracs.IBTrACS(CONFIG, period, region, passwd)
+        # ibtracs_ = ibtracs.IBTrACS(CONFIG, period, region, passwd)
         era5_ = era5.ERA5Manager(CONFIG, period, region, passwd)
         # cwind_ = cwind.CwindManager(CONFIG, period, region, passwd)
         # stdmet_ = stdmet.StdmetManager(CONFIG, period, region, passwd)
